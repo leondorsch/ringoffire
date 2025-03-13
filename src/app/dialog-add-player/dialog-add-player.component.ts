45d3a0f12e7668b30813d10419c3fa6a526764b2
Leon Dorsch <leon.dorsch@gmx.de>
2025-03-13 15:05:09 +0100

    first playable version

---
+++ src/app/dialog-add-player/dialog-add-player.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+
+
+@Component({
+  selector: 'app-dialog-add-player',
+  templateUrl: './dialog-add-player.component.html',
+  styleUrls: ['./dialog-add-player.component.scss']
+})
+export class DialogAddPlayerComponent {
+  name: string = '';
+
+  constructor(public dialogRef: MatDialogRef<DialogAddPlayerComponent>) { }
+  onNoClick() {
+    this.dialogRef.close();
+  }
+
+}
---
+++ src/app/game/game.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { Game } from 'src/models/game';
+import { MatDialog } from "@angular/material/dialog";
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss']
+})
+export class GameComponent {
+  pickCardAnimation = false;
+  game: Game | any;
+  currentCard: any;
+
+  constructor(public dialog: MatDialog) {
+    this.newGame();
+    console.log(this.game)
+  }
+
+  newGame() {
+    this.game = new Game();
+  }
+
+  takeCard() {
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      this.pickCardAnimation = true;
+      this.game.currentPlayer++;
+      this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.pickCardAnimation = false;
+      }, 1000);
+    }
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent, {
+    });
+
+    dialogRef.afterClosed().subscribe(name => {
+      if (name && name.length > 0) {
+        this.game.players.push(name);
+      }
+    });
+  }
+}
+
